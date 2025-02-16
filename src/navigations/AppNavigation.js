059d18671989917f5a9bcb8ccecc199cbf2b927d
aghmnl <aghmnl@gmail.com>
2025-02-16 12:22:51 -0300

    77. Añadiendo screen globales

--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -1,6 +1,10 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { BottomTabNavigation } from "./BottomTabNavigation";
+import { UserProfileScreen, CameraScreen, ImageFullScreen } from "../screens/Global";
+import { ChatScreen } from "../screens/Chats";
+import { GroupScreen, GroupProfileScreen, AddUserGroupScreen, ChangeNameGroupScreen } from "../screens/Groups";
 import { screens } from "../utils";
+import { styles } from "../navigations";
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +12,16 @@ export function AppNavigation() {
   return (
     <Stack.Navigator>
       <Stack.Screen name={screens.tab.root} component={BottomTabNavigation} options={{ headerShown: false }} />
+      <Stack.Screen name={screens.global.chatScreen} component={ChatScreen} options={{ headerShown: false, ...styles.stackNavigationStyles }} />
+      <Stack.Screen name={screens.global.groupScreen} component={GroupScreen} options={{ headerShown: false, ...styles.stackNavigationStyles }} />
+      <Stack.Group screenOptions={{ presentation: "modal", ...styles.modalStyles }}>
+        <Stack.Screen name={screens.global.userProfileScreen} component={UserProfileScreen} options={{ title: "Info. del usuario" }} />
+        <Stack.Screen name={screens.global.groupProfileScreen} component={GroupProfileScreen} options={{ title: "Info. del grupo" }} />
+        <Stack.Screen name={screens.global.addUserGroupScreen} component={AddUserGroupScreen} options={{ title: "Añadir participante" }} />
+        <Stack.Screen name={screens.global.changeNameGroupScreen} component={ChangeNameGroupScreen} options={{ title: "Cambiar nombre del grupo" }} />
+        <Stack.Screen name={screens.global.cameraScreen} component={CameraScreen} options={{ headerShown: false }} />
+        <Stack.Screen name={screens.global.imageFullScreen} component={ImageFullScreen} options={{ headerShown: false }} />
+      </Stack.Group>
     </Stack.Navigator>
   );
 }
