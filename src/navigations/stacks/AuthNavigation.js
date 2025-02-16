f8041af619417f83192cdf3cbc825b45ca8e6e19
aghmnl <aghmnl@gmail.com>
2025-02-16 07:57:46 -0300

    71. Icono back para las screens

--- src/navigations/stacks/AuthNavigation.js
+++ src/navigations/stacks/AuthNavigation.js
@@ -1,12 +1,13 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthStartScreen, LoginScreen, RegisterScreen } from "../../screens/Auth";
+import { IconBack } from "../../components/Navigation";
 import { screens } from "../../utils";
 
 const Stack = createNativeStackNavigator();
 
 export function AuthNavigation() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerLeft: IconBack }}>
       <Stack.Screen name={screens.auth.authStartScreen} component={AuthStartScreen} options={{ headerShown: false }} />
       <Stack.Screen name={screens.auth.loginScreen} component={LoginScreen} options={{ title: "Iniciar sesión" }} />
       <Stack.Screen name={screens.auth.registerScreen} component={RegisterScreen} options={{ title: "Registro" }} />
