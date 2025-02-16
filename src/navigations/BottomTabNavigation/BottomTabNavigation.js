bef2cea79dbd07dbeeb2fd58c6538d6b8c454af6
aghmnl <aghmnl@gmail.com>
2025-02-16 12:06:38 -0300

    76. Añadiendo iconos al menú

--- src/navigations/BottomTabNavigation/BottomTabNavigation.js
+++ src/navigations/BottomTabNavigation/BottomTabNavigation.js
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "native-base";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { ChatsNavigation, GroupsNavigation, SettingsNavigation } from "../stacks";
 import { screens } from "../../utils";
 import { styles } from "./BottomTabNavigation.styles";
@@ -10,11 +12,12 @@ export function ButtomTabNavigation() {}
 export function BottomTabNavigation() {
   return (
     <Tab.Navigator
-      screenOptions={() => ({
+      screenOptions={({ route }) => ({
         headerShown: false,
         tabBarStyle: styles.tabBarStyle,
         tabBarInactiveTintColor: "#646464",
         tabBarActiveTintColor: "#0891b2",
+        tabBarIcon: ({ color, size }) => screenIcon(route, color, size),
       })}
     >
       <Tab.Screen name={screens.tab.chats.root} component={ChatsNavigation} options={{ title: "Chats" }} />
@@ -22,4 +25,20 @@ export function BottomTabNavigation() {
       <Tab.Screen name={screens.tab.settings.root} component={SettingsNavigation} options={{ title: "Ajustes" }} />
     </Tab.Navigator>
   );
+}
+
+function screenIcon(route, color, size) {
+  let iconName;
+
+  if (route.name === screens.tab.chats.root) {
+    iconName = "chat";
+  }
+  if (route.name === screens.tab.groups.root) {
+    iconName = "account-group";
+  }
+  if (route.name === screens.tab.settings.root) {
+    iconName = "cog-outline";
+  }
+
+  return <Icon as={MaterialCommunityIcons} name={iconName} color={color} size={size} />;
 }
