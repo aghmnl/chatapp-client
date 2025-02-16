71602f3a82b939a4159076c966fe949d0cbff37c
aghmnl <aghmnl@gmail.com>
2025-02-16 18:32:35 -0300

    79. Custom hooks useAuth - testing useAuth

--- src/screens/Auth/AuthStartScreen/AuthStartScreen.js
+++ src/screens/Auth/AuthStartScreen/AuthStartScreen.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useAuth } from "../../../hooks";
 import { styles } from "../../../navigations";
 
 export function AuthStartScreen() {
+  const auth = useAuth();
+
+  console.log("auth", auth);
+
   return (
     <View style={styles.marginTop}>
       <Text style={{ color: "#fff" }}>AuthStartScreen</Text>
