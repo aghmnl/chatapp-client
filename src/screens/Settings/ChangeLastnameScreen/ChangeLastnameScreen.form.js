4021d23fdb0e5ab81ad56183b966a37efda3106f
aghmnl <aghmnl@gmail.com>
2025-02-19 15:36:05 -0300

    104. Formulario para cambiar apellidos

---
+++ src/screens/Settings/ChangeLastnameScreen/ChangeLastnameScreen.form.js
@@ -0,0 +1,9 @@
+import * as Yup from "yup";
+
+export function initialValues() {
+  return { lastname: "" };
+}
+
+export function validationSchema() {
+  return Yup.object({ lastname: Yup.string().required(true) });
+}
