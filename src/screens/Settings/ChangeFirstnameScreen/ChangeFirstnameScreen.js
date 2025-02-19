500b702439917ce2e881db8b952a23067eea8c42
aghmnl <aghmnl@gmail.com>
2025-02-19 15:18:45 -0300

    102. Validando y controlando formulario

--- src/screens/Settings/ChangeFirstnameScreen/ChangeFirstnameScreen.js
+++ src/screens/Settings/ChangeFirstnameScreen/ChangeFirstnameScreen.js
@@ -1,12 +1,37 @@
 import { View } from "react-native";
 import { Input, Button } from "native-base";
+import { useFormik } from "formik";
+import { initialValues, validationSchema } from "./ChangeFirstnameScreen.form";
 import { styles } from "./ChangeFirstnameScreen.styles";
 
 export function ChangeFirstnameScreen() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validationSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      try {
+        console.log(formValue);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
   return (
     <View style={styles.content}>
-      <Input placeholder="Nombre" variant="unstyled" autoFocus style={styles.input} />
-      <Button style={styles.btn}>Cambiar</Button>
+      <Input
+        placeholder="Nombre"
+        variant="unstyled"
+        autoFocus
+        value={formik.values.firstname}
+        onChangeText={(text) => formik.setFieldValue("firstname", text)}
+        style={[styles.input, formik.errors.firstname && styles.inputError]}
+      />
+
+      <Button style={styles.btn} onPress={formik.handleSubmit} isLoading={formik.isLoading}>
+        Cambiar
+      </Button>
     </View>
   );
 }
